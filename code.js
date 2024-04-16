01ce7324b7fa0a946918f5c9bf812e0667843c94
j-halladay5 <156850724+j-halladay5@users.noreply.github.com>
2024-04-16 03:03:08 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,30 @@
-function augmentingPath(graph, start, end) {
+function augmentingPath(graph, start, end, path = [])
+{
+    path.push(start);
+    if(start == end)
+    {
+        return path;
+    }
+    
+    for(var key in graph[start])
+    {
+        if(!path.includes(key))
+        {
+            if(key == end)
+            {
+                path.push(key);
+                return path;
+            }
+            
+            var newPath = augmentingPath(graph, key, end, path);
+            if(newPath.length > 0)
+            {
+                return newPath;
+            }
+            
+        }
+    }
     return [];
 }
+
+
